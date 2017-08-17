439d74440eb707daed9fc5b6008ffa65531e9e7c
Jc337839 <denzel.getaruelas@my.jcu.edu.au>
2017-08-17 18:57:57 +1000

    Debugging completed

---
+++ DebuggingExercises/FixDebugThree3.java
@@ -0,0 +1,22 @@
+// This application gets a user's name and displays a greeting
+import java.util.Scanner;
+public class FixDebugThree3
+{
+   public static void main(String args[])
+   {
+      String name = getName();
+      displayGreeting(name);
+   }
+   private static String getName()
+   {
+      String name;
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter name" );
+      name = input.nextLine();
+      return name;
+   }
+   private static void displayGreeting(String name)
+   {
+      System.out.println("Hello, " + name + "!");
+   }
+}
